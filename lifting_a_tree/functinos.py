5809bc798b69a8195003df418e1e5ccea224664b
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-03-05 13:36:50 +0100

    new stuff

--- lifting_a_tree/functinos.py
+++ lifting_a_tree/functinos.py
@@ -1,22 +1,86 @@
-from math import pi
+"""
+Functions representing the formulas in the article 
+"""
 
+from math import pi, sin, cos, acos, sqrt
 
-def cone_vol(r : float, R : float, h : float) -> float:
+# Constants
+G = 9.81
+
+# Cone functions
+def trun_cone_vol(r : float, R : float, h : float) -> float:
+    """Returns the volume of a truncated cone
+
+    Args:
+        r (float): Upper radius of the cone
+        R (float): Lower radius of the cone
+        h (float): Height of the cone
+
+    Returns:
+        float: The volume
+    """
     return h * pi / 3 * (r**2 + R*r + R**2)
 
 
-def cone_point(r : float, R : float, h : float) -> float:
-    h2 = h * R / (R - r)
-    h1 = h2 - h
-    
-    p1 = h1 / 3 + h
-    p2 = h2 / 3
-    
-    v2 = cone_vol(0, R, h2)
-    v1 = cone_vol(0, r, h1)
-    
-    return (v2 * p2 - v1 * p1) / (v2 + v1)
-    
-"""
-def motor_torque(v : float) -> float:
+def trun_cone_com(r : float, R : float, h : float) -> float:
+    """Returns the center of mass of a truncated cone
+
+    Args:
+        r (float): _description_
+        R (float): _description_
+        h (float): _description_
+
+    Returns:
+        float: _description_
     """
+    return h * (R**2/2 + R*(r-R)*2/3 + (r-R)**2/4) / (R**2 + R*(r-R) + (r-R)**2/3)
+
+
+# Geometrical shape
+def alpha_t(beta : float) -> float:
+    return pi/2 - beta
+
+
+def beta_t(alpha : float) -> float:
+    return pi/2 - alpha
+
+
+def gamma_t(alpha : float) -> float:
+    return (pi - alpha) / 2
+    
+    
+def cyl_angle_from_length(h_com : float, length : float) -> float:
+    return acos(1 - 2*length**2 / h_com**2)
+
+
+def cyl_length_from_angle(h_com : float, alpha : float) -> float:
+    return h_com * sqrt((1 - cos(alpha))/2)
+
+
+# Motor torque functinos
+def inertia_tree(h : float, m : float) -> float:
+    return h**2 * m / 3
+
+
+def inertia_rod(i_t : float, h_com : float) -> float:
+    return 4 * i_t / h_com**2 
+
+
+def inertia_motor(i_r : float, spec_pitch : float) -> float:
+    return i_r * spec_pitch**2
+
+
+def tree_load_torque(h_com : float, m : float, beta : float) -> float:
+    return h_com * m * G * cos(beta)
+
+
+def cylinder_load_force(t_tree : float, h_com : float, gamma : float) -> float:
+    return 2 * t_tree / h_com / sin(gamma)
+
+
+def motor_load_force(f_cyl : float, pitch : float) -> float:
+    return f_cyl * pitch
+
+# Motor torque curves
+def motor_torque(omega : float) -> float:
+    return 1.0 - omega 
--- lifting_a_tree/main.py
+++ lifting_a_tree/main.py
@@ -1,5 +1,6 @@
 from functinos import *
 from math import asin, pi
+import matplotlib.pyplot as plt
 
 # Constants
 CAT_NUM = 16
@@ -7,8 +8,9 @@ CAT_NUM = 16
 DIAMETER_LOWER = 0.6
 DENSITY = 600
 BETA_MAX = pi / 2
+SPINDLE_PITCH = 8 / 1
 
-G = 9.81
+DELTA_TIME = 0.01    # Defines accuracy
 
 # Parameters
 DIAMETER_UPPER = 0.2 - CAT_NUM / 300
@@ -17,28 +19,102 @@ LIFTING_TIME = 33 + CAT_NUM * 3
 
 START_HEIGHT = 1
 
-print("Calculations: ")
+# General
+spec_pitch = SPINDLE_PITCH / 1000 / pi / 2
 
-# Calculation
-v_col = cone_vol(DIAMETER_UPPER / 2, DIAMETER_LOWER / 2, HEIGHT_TREE)
+# Cone
+v_col = trun_cone_vol(DIAMETER_UPPER / 2, DIAMETER_LOWER / 2, HEIGHT_TREE)
 m_tree = v_col * DENSITY
 
-print("Volume:", v_col, "m³")
-print("Mass:", m_tree, "kg")
+print("Volume: \t\t\t", v_col, "m³")
+print("Mass: \t\t\t\t", m_tree, "kg")
 
-h_s = 12.26 # cone_point(DIAMETER_UPPER / 2, DIAMETER_LOWER / 2, HEIGHT_TREE)
+h_com = trun_cone_com(DIAMETER_UPPER / 2, DIAMETER_LOWER / 2, HEIGHT_TREE)
 
-print("Center of mass height:", h_s, "m")
+print("Center of mass height:\t\t", h_com, "m")
 
-beta_0 = asin(1 / h_s)
-delta_z_s = h_s - START_HEIGHT
+beta_0 = asin(1 / h_com)
+delta_z_s = h_com - START_HEIGHT
 
 e_req = m_tree * G * delta_z_s
 p_req = e_req / LIFTING_TIME
 
-print("Required energy:", p_req, "W")
+print("Average theoretical power:\t", p_req, "W")
 
 delta_beta = BETA_MAX - beta_0
 omega_av = delta_beta / LIFTING_TIME
 omega_max = omega_av * 2
 
+print("Average angluar velocity: \t", omega_av, "rad/s")
+
+# Inertias
+i_tree = inertia_tree(HEIGHT_TREE, m_tree)
+i_rod = inertia_rod(i_tree, h_com)
+i_motor = inertia_motor(i_rod, spec_pitch)
+
+print("Tree inertia: \t\t\t", i_tree, "kgm²")
+print("Rod inertia: \t\t\t", i_rod, "kg")
+print("Motor inertia: \t\t\t", i_motor, "kgm²")
+
+# Load
+t_tree_max = tree_load_torque(h_com, m_tree, beta_0)
+f_cyl_max = cylinder_load_force(t_tree_max, h_com, gamma_t(alpha_t(beta_0)))
+t_mot_max = motor_load_force(f_cyl_max, spec_pitch)
+
+print("Max motor torque: \t\t", t_mot_max, "Nm")
+
+# Plotting
+distance_0 = cyl_length_from_angle(h_com, alpha_t(beta_0))
+
+velocities = [ 0.0 ]
+phis = [ 0.0 ]
+distances = [ distance_0 ]
+accelerations = [ 0.0 ]
+torques = [ 0.0 ]
+
+betas = [ beta_0 ]
+alphas = [ alpha_t(beta_0) ]
+gammas = [ gamma_t(alphas[-1]) ]
+
+xdata = [ 0.0 ]
+
+while True:
+    try:
+        velocity = velocities[-1]
+        torque = motor_torque(velocity) - motor_load_force(cylinder_load_force(tree_load_torque(h_com, m_tree, betas[-1]), h_com, gammas[-1]), spec_pitch)
+        acceleration = torque / i_motor
+        velocity = (acceleration + accelerations[-1]) / 2 * DELTA_TIME
+        phi = phis[-1] + (velocity + velocities[-1]) / 2 * DELTA_TIME 
+        distance = distance_0 - phi * spec_pitch
+        
+        alpha = cyl_angle_from_length(h_com, distance)
+        beta = beta_t(alpha)
+        gamma = gamma_t(alpha)
+        
+        if distance < 0:
+            alpha = 0
+            beta = pi / 2
+            gamma = 0
+        
+        velocities.append(velocity)
+        torques.append(torque)
+        accelerations.append(acceleration)
+        phis.append(phi)
+        distances.append(distance)
+        alphas.append(alpha)
+        betas.append(beta)
+        gammas.append(gamma)
+        
+        if beta >= (pi / 2):
+            break
+        
+        xdata.append(xdata[-1] + DELTA_TIME)
+    except Exception as e:
+        print("Error in iteration number:", len(xdata))
+        print(e)
+        break
+
+
+# plt.plot(xdata[0:len(velocities)], velocities, "red")
+plt.plot(xdata[0:len(velocities)], betas)
+plt.show()
